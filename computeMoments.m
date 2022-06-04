c3833578e9766de97c2448bf5f9c328a1cb59cb9
vianneydubois <vianneydubois@hotmail.fr>
2022-06-04 02:59:24 +0200

    starboard/port separation for rudders and daggerboards. Runs but not fully tested

--- computeMoments.m
+++ computeMoments.m
@@ -26,22 +26,28 @@ d_MAC = 2/3 * d_root * (1+d_taper+d_taper^2) / (1+d_taper);
 d_OP_p = d_le_root + [-1/3 * d_MAC 0 1/2*d_length];
 d_GP_p = -GO + d_OP_p;
 % starboard daggerboard
+d_OP_s = d_le_root .*[1 -1 1] + [-1/3 * d_MAC 0 1/2*d_length];
+d_GP_s = -GO + d_OP_s;
 
 
 % rudder (appl. point is considered at 1/3 of the MAC (@ aero centre))
 r_taper = r_tip/r_root;
 r_MAC = 2/3 * r_root * (1+r_taper+r_taper^2) / (1+r_taper);
-
-r_OP = r_le_root - 1/3 * r_MAC * [cosd(-delta_r) sind(-delta_r) 0] ...
+% port rudder
+r_OP_p = r_le_root - 1/3 * r_MAC * [cosd(-delta_r) sind(-delta_r) 0] ...
     + [0 0 1/2*r_length];
-r_GP = -GO + r_OP;
+r_GP_p = -GO + r_OP_p;
+% starboard rudder
+r_OP_s = r_le_root .* [1 -1 1] - 1/3 * r_MAC * [cosd(-delta_r) sind(-delta_r) 0] ...
+    + [0 0 1/2*r_length];
+r_GP_s = -GO + r_OP_s;
 
 % hulls yawing moments are negligated
 
 %% Yawing moments
 Ms = cross(s_GP, Fs)';
-Md = 2 * cross(d_GP_p, Fd)';
-Mr = 2 * cross(r_GP, Fr)';
+Md = cross(d_GP_p, Fd)' + cross(d_GP_s, Fd)';
+Mr = cross(r_GP_p, Fr)' + cross(r_GP_s, Fr)';
 Mh = 2 * 0 * Fh;
 
 end
